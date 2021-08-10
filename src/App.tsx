02e9d68db3faf26e1652babd4879edfb443b0b58
Arthur Luciano <arthurluciano7@gmail.com>
2021-08-10 10:17:08 -0300

    refactor: refatorando arquivos gerais

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import { Dashboard } from "./ui/Dashboard";
 import { Insert } from "./ui/Insert";
-import { Route, BrowserRouter } from "react-router-dom";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
 import { SearchEdit } from "./ui/SearchEdit";
 import { Edit } from "./ui/Edit";
+import { Login } from "./ui/Login";
+import { Register } from "./ui/Register";
+import { LandingPage } from "./ui/LandingPage";
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/dashboard" exact component={Dashboard}/>
-      <Route path="/insert" exact component={Insert}/>
-      <Route path="/edit" exact component={SearchEdit}/>
-      <Route path="/edit/:id" exact component={Edit}/>
+      <Switch>
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/insert" component={Insert} />
+        <Route path="/edit" component={SearchEdit} />
+        <Route path="/edit/:id" component={Edit} />
+      </Switch>
     </BrowserRouter>
   );
 }
