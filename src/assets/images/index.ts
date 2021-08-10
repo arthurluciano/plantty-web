2bf9bf869afc23ad3a429285279b1a954d3901e5
Arthur Luciano <arthurluciano7@gmail.com>
2021-08-10 10:13:32 -0300

    feat: iniciando o arquivo global de imagens para fácil acesso

---
+++ src/assets/images/index.ts
@@ -0,0 +1,22 @@
+import plantIcon from '../../assets/images/Global/plant-logo.svg';
+import arrow from '../../assets/images/LandingPage/arrow.svg';
+import palmPlant from '../../assets/images/LandingPage/palm-plant.svg';
+import dibble from '../../assets/images/LandingPage/dibble.svg';
+import reap from '../../assets/images/LandingPage/reap.svg';
+import vase from '../../assets/images/LandingPage/vase.svg';
+import app from '../../assets/images/LandingPage/app.svg';
+import avaliation1 from '../../assets/images/LandingPage/avaliation1.svg';
+import avaliation2 from '../../assets/images/LandingPage/avaliation2.svg';
+import avaliation3 from '../../assets/images/LandingPage/avaliation3.svg';
+import appleStore from '../../assets/images/LandingPage/apple-store.svg';
+import googlePlay from '../../assets/images/LandingPage/google-play.svg';
+import miStore from '../../assets/images/LandingPage/mi-store.svg';
+import huaweiStore from '../../assets/images/LandingPage/huawei-store.svg';
+import contact from '../../assets/images/LandingPage/contact.svg';
+import instagram from '../../assets/images/LandingPage/instagram.svg';
+import twitter from '../../assets/images/LandingPage/twitter.svg';
+import facebook from '../../assets/images/LandingPage/facebook.svg';
+import email from '../../assets/images/LandingPage/email.svg';
+
+export {plantIcon, arrow, palmPlant, dibble, reap, vase, app, avaliation1, avaliation2, 
+  avaliation3, appleStore, googlePlay, miStore, huaweiStore, contact, instagram, twitter, facebook, email};
